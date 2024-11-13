f56ece083f2fa32ae252d904575a2aa1a537f6f1
ipipi2024 <ipulepipi60@gmail.com>
2024-11-12 23:27:45 -0500

    add darl mode to category and update

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -69,10 +69,10 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
           ))}
 
         <AlertDialog>
-          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
+          <AlertDialogTrigger className="dark:hover:bg-gray-900 p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
             Add new category
           </AlertDialogTrigger>
-          <AlertDialogContent className="bg-white">
+          <AlertDialogContent className="dark:bg-gray-900 bg-white">
             <AlertDialogHeader>
               <AlertDialogTitle>New Category</AlertDialogTitle>
               <AlertDialogDescription>
